ba3209e5f1e4b34a7651988eb981bd73114a8f5f
Elb33 <layeniboy14@gmail.com>
2023-03-03 21:20:26 +0100

    coherent name changing

---
+++ src/Speech-to-Text.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const App = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Your Browser doesn't support</span>;
+  }
+
+  return (
+    <div className="speech">
+      <p className="mic-status">Microphone: {listening ? "ON" : "OFF"}</p>
+      <button className="start" onClick={SpeechRecognition.startListening}>
+        Start
+      </button>
+      <button className="reset" onClick={resetTranscript}>
+        Reset
+      </button>
+      <button className="stop" onClick={SpeechRecognition.stopListening}>
+        Stop
+      </button>
+      {/* <Translate /> */}
+      <p className="text">{transcript}</p>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import Translate from "./translate";
+import App from "./Speech-to-Text";
+import Translate from "./Text-to-Speech";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
